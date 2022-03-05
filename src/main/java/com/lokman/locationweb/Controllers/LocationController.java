8386fe67dc5f31dfe48e9e6444d14789a456bc7b
user <lhossainduet094026.gmail.com>
2022-03-06 00:19:33 +0600

    --application context added in application.properties. --saveLocation controller method added in LocationController.

--- src/main/java/com/lokman/locationweb/Controllers/LocationController.java
+++ src/main/java/com/lokman/locationweb/Controllers/LocationController.java
@@ -1,13 +1,33 @@
 package com.lokman.locationweb.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.lokman.locationweb.Services.LocationService;
+import com.lokman.locationweb.entities.Location;
 
 @Controller
 public class LocationController {
 
+	@Autowired
+	LocationService locationService;
+
 	@GetMapping("/showCreate")
 	public String createLocation() {
 		return "createLocation";
+	}
+
+	@PostMapping("/saveloc")
+	public String saveLocation(@ModelAttribute("location") Location location, ModelMap map) {
+
+		Location locaton = locationService.saveLocaton(location);
+		String successMessage = "Location with Id:" + locaton.getId() + " successfully saved.";
+		map.addAttribute("successMessage", successMessage);
+		return "createLocation";
+
 	}
 }
--- src/main/webapp/WEB-INF/jsps/createLocation.jsp
+++ src/main/webapp/WEB-INF/jsps/createLocation.jsp
@@ -1,21 +1,22 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
 	pageEncoding="ISO-8859-1"%>
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jstl/core"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="ISO-8859-1">
 <title>Save location</title>
-<form action="" method="post">
+<form action="saveloc" method="post">
 	<pre>
 	Id:<input type="text" name="id"> 
 	Code:<input type="text" name="code">
 	Name:<input type="text" name="name"> 
-	Type: Urban<input type="radio" name="type">
-	Rural:<input type="radio" name="type">
+	Type: Urban<input type="radio" name="type" value="URBAN">
+	Rural:<input type="radio" name="type" value="RURAL">
 	<input type="submit" value="Save">
 	</pre>
 </head>
-<body>
-
+<body>${successMessage}
 </body>
 </html>
